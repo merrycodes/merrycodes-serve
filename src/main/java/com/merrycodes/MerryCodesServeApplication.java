d25d9d2409439506e512affdeff340e77aea08b1
鲁涛 <1583334209@qq.com>
2020-03-30 13:01:28 +0800

    init merrycodes-serve

---
+++ src/main/java/com/merrycodes/MerryCodesServeApplication.java
@@ -0,0 +1,17 @@
+package com.merrycodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Miracle.Lt
+ * @date 2020/3/30 12:15
+ */
+@SpringBootApplication
+public class MerryCodesServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerryCodesServeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/merrycodes/MerrycodesServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merrycodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerrycodesServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
