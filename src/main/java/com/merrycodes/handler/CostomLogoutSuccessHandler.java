bd72b36ccc6418af39bddfd12fecc367b9cdff1e
鲁涛 <merrycodes@163.com>
2020-05-15 11:35:34 +0800

    :bug:找回误删文件

---
+++ src/main/java/com/merrycodes/handler/CostomLogoutSuccessHandler.java
@@ -0,0 +1,29 @@
+package com.merrycodes.handler;
+
+import com.merrycodes.utils.JsonUtils;
+import com.merrycodes.utils.ResponseUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 自定义登出成功处理器
+ *
+ * @author MerryCodes
+ * @date 2020/5/8 16:13
+ */
+@Slf4j
+@Component
+public class CostomLogoutSuccessHandler implements LogoutSuccessHandler {
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
+                                Authentication authentication) throws IOException {
+        String jsonString = JsonUtils.writeValue(ResponseUtils.success("登出成功")).orElseThrow(NullPointerException::new);
+        ResponseUtils.response(response, jsonString);
+    }
+}
